f0f41fc69c1e13fd390ca06fb297073cbc2a2a6c
sai kumar <msaikumar31@gmail.com>
2024-09-26 18:46:25 +0530

    updated db seed for timeslots

--- patient-management-app/database/seeders/AppointmentsTableSeeder.php
+++ patient-management-app/database/seeders/AppointmentsTableSeeder.php
@@ -14,13 +14,14 @@ class AppointmentsTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
+        $allSlots = ['10:00:00', '12:00:00', '14:00:00', '16:00:00', '18:00:00', '22:00:00'];
 
         foreach (range(1, 50) as $index) {
             Appointment::create([
                 'patient_id' => Patient::inRandomOrder()->first()->id,
                 'doctor_id' => Doctor::inRandomOrder()->first()->id,
                 'date' => Carbon::now()->addDays($faker->numberBetween(1, 10)),
-                'time' => $faker->time('H:i:s'),
+                'time' => $faker->randomElement($allSlots), // Randomly select from the predefined slots
                 'status' => $faker->randomElement(['Booked', 'Completed', 'Canceled']),
             ]);
         }
